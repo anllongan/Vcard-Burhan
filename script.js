aa2b5aa078761132813bafed655b6fd989369bf0
Anil Ongan <126762587+anllongan@users.noreply.github.com>
2024-01-16 11:59:24 +0300

    Qr code updates design updated (icons,font size,collapse card replace)

--- script.js
+++ script.js
@@ -10,11 +10,38 @@ saveBtn.addEventListener("click", function () {
   var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
   var blob = new Blob([vcard], { type: "text/vcard" });
   var url = URL.createObjectURL(blob);
-  
+
   const newLink = document.createElement('a');
   newLink.download = contact.name + ".vcf";
   newLink.textContent = contact.name;
   newLink.href = url;
-  
+
   newLink.click();
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  var qrButton = document.getElementById("qrButton");
+  var qrCollapse = document.getElementById("qrCollapse");
+
+  // Show QR modal when button is clicked
+  qrButton.addEventListener("click", function () {
+    if (qrCollapse.classList.contains("show")) {
+      qrCollapse.classList.remove("show");
+    } else {
+      qrCollapse.classList.add("show");
+    }
+  });
+
+  // Close QR modal on outside click
+  document.addEventListener("click", function (event) {
+    if (!qrButton.contains(event.target) && !qrCollapse.contains(event.target)) {
+      qrCollapse.classList.remove("show");
+    }
+  });
+
+  // Close QR modal on scroll
+  window.addEventListener("scroll", function () {
+    qrCollapse.classList.remove("show");
+  });
 });
